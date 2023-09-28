5117c24770727b25e531f995ea63cfda505bbffb
taka <takahashi.akira.36m@gmail.com>
2023-09-28 11:34:04 +0900

    Modify path

--- example_stam_m_2023/d1_priority_setter_blox.zsh
+++ example_stam_m_2023/d1_priority_setter_blox.zsh
@@ -7,6 +7,6 @@
 #$ -pe all_pe* 36
 
 ml_atomate_path=${HOME}/ml_atomate
-python ${ml_atomate_path}/ml_atomate/priority_setter.py -df ~/fireworks_config_atomate/db.json -bld ${ml_atomate_path}/example_stam_m_2023Sep/atomate_files/run_builder.py -dc descriptors.csv --objective bandstructure_hse.bandgap dielectric.epsilon_avg -rs 0 -ad -nrb 0 -nws 20 -c no_conversion log --blox
+python ${ml_atomate_path}/ml_atomate/priority_setter.py -df ~/fireworks_config_atomate/db.json -bld ${ml_atomate_path}/example_stam_m_2023/atomate_files/run_builder.py -dc descriptors.csv --objective bandstructure_hse.bandgap dielectric.epsilon_avg -rs 0 -ad -nrb 0 -nws 20 -c no_conversion log --blox
 
 
--- example_stam_m_2023/d2_priority_setter_ptr.zsh
+++ example_stam_m_2023/d2_priority_setter_ptr.zsh
@@ -7,5 +7,5 @@
 #$ -pe all_pe* 36
 
 ml_atomate_path=${HOME}/ml_atomate
-python ${ml_atomate_path}/ml_atomate/priority_setter.py -df ~/fw_config/db.json -bld ${ml_atomate_path}/example_stam_m_2023Sep/atomate_files/run_builder.py -dc descriptors.csv --objective bandstructure_hse.bandgap 4.0, dielectric.epsilon_avg 30.0, -rs 0 -ad -nrb 0 -nws 20 -c no_conversion log
+python ${ml_atomate_path}/ml_atomate/priority_setter.py -df ~/fw_config/db.json -bld ${ml_atomate_path}/example_stam_m_2023/atomate_files/run_builder.py -dc descriptors.csv --objective bandstructure_hse.bandgap 4.0, dielectric.epsilon_avg 30.0, -rs 0 -ad -nrb 0 -nws 20 -c no_conversion log
 
